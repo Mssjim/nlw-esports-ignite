61dce47d85aba6b861b08e7a710e71f41dca20da
Mssjim <mrmssjim@gmail.com>
2022-09-12 23:25:52 -0300

    index page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: [
+    './src/**/*.tsx',
+    'index.html'
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      colors: {
+      },
+      backgroundImage: {
+        'nlw-gradient': 'linear-gradient(89.86deg, #8572FC 24.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)',
+        galaxy: "url('/background-galaxy.png')"
+      }
+    },
   },
   plugins: [],
 }
