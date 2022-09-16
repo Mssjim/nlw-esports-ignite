9379577fa0c8ae71d87a3ae62e63f281a34155de
Mssjim <mrmssjim@gmail.com>
2022-09-16 13:35:44 -0300

    Add: create default routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
 
-app.get('/ads', (request, response)=>{
-    return response.json([
-        {id: 1, name: 'Anuncio 1'},
-        {id: 2, name: 'Anuncio 2'},
-        {id: 3, name: 'Anuncio 3'},
-        {id: 4, name: 'Anuncio 4'},
-        {id: 5, name: 'Anuncio 5'},
-    ])
-})
+app.get('/games', (req, res) => {
+    return res.status(200).json([]);
+});
 
-app.listen(3333)
+app.get('/games/:id/ads', (req, res)=>{
+    const id = req.params.id;
+    return res.status(200).json([]);
+});
+
+app.post('/ads', (req, res) => {
+    return res.status(201).send([]);
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+    return res.status(200).send([]);
+});
+
+app.listen(3333);
