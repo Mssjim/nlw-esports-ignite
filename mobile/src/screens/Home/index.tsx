1d245fdfd7538aa277ec1790e28440444e56704a
Mssjim <mrmssjim@gmail.com>
2022-09-19 18:41:18 -0300

    Ops: get games from server

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,37 +1,44 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
-
-import { GAMES } from '../../utils/games';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={logoImg}
-        style={styles.logo}
-      />
+	const [games, setGames] = useState<GameCardProps[]>([]);
 
-      <Heading
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..." 
-      />
+	useEffect(() => {
+		fetch('http://192.168.1.21:3333/games')
+			.then(res => res.json())
+			.then(data => setGames(data))
+	}, []);
 
-      <FlatList
-        contentContainerStyle={styles.contentList}
-        data={GAMES}
-        keyExtractor={item => item.id}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        renderItem={({item}) => (
-          <GameCard
-          data={item}
-        />
-        )}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+		<Image
+			source={logoImg}
+			style={styles.logo}
+		/>
+
+		<Heading
+			title="Encontre seu duo!"
+			subtitle="Selecione o game que deseja jogar..." 
+		/>
+
+		<FlatList
+			contentContainerStyle={styles.contentList}
+			data={games}
+			keyExtractor={item => item.id}
+			showsHorizontalScrollIndicator={false}
+			horizontal
+			renderItem={({item}) => (
+			<GameCard
+			data={item}
+			/>
+			)}
+		/>
+		</View>
+	);
 }
